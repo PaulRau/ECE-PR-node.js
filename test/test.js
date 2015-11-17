551a7f6fdd0c86983a763e15596bf10672a05111
PaulECE <rauber@ece.fr>
2015-11-17 12:23:16 +0100

    correction

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var users = require('../lib/users.js');
 
 describe('User tests', function() {
   it('should save user', function(done) {
-    
+
       var testUser = { id: "1111", name: "Sterling" };
 
       users.save(user.name, function (testUser) {
@@ -14,3 +14,4 @@ describe('User tests', function() {
       done();
     });
   });
+;
