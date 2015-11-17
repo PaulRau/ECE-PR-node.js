62b6f5a3c2940cdb6d05f44a5646c80200766246
Belteshazzar89 <sterling.pierson.coleman@gmail.com>
2015-11-17 11:38:07 +0100

    Moved correct test.js into bin directory

--- bin/test.js
+++ bin/test.js
@@ -1,4 +1,8 @@
 var should = require('should');
-describe('User tests', function()) {
-  
-}
+describe('User tests', function() {
+  it('should save user', function(done) {
+    var user = new User('22');
+    user.save(done);
+
+    });
+  });
--- test.js
+++
@@ -1,8 +0,0 @@
-var should = require('should');
-describe('User tests', function() {
-  it('should save user', function(done) {
-    var user = new User('22');
-    user.save(done);
-
-    });
-  });
